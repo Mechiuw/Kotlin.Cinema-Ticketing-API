b4e23ce931fcc22a29da2b015c0f77b93b1abe79
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-17 23:22:56 +0700

    [THEATER][FEATURE] Defined Theater entity

---
+++ src/main/kotlin/com/mcsoftware/ticketo/theater/model/entity/Theater.kt
@@ -0,0 +1,29 @@
+package com.mcsoftware.ticketo.theater.model.entity
+
+import com.fasterxml.jackson.annotation.JsonBackReference
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.util.UUID
+
+data class Theater(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "theater_id", updatable = false, nullable = false)
+    val id:UUID,
+
+    @Column(name = "theater_number", nullable = false)
+    var theaterNumber:String,
+
+    @Column(name = "stock_seats", nullable = false)
+    val stockSeats:Int,
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "film_id", referencedColumnName = "id", nullable = false)
+    val filmId:UUID,
+
+)
