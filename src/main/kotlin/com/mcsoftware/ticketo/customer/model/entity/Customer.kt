9386114d202766d800b6f185f2a33ca25cf37342
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-17 16:06:34 +0700

    [CUSTOMER][FEATURE] Defined Entity and its packages

---
+++ src/main/kotlin/com/mcsoftware/ticketo/customer/model/entity/Customer.kt
@@ -0,0 +1,25 @@
+package com.mcsoftware.ticketo.customer.model.entity
+
+import jakarta.persistence.*
+import org.springframework.stereotype.Service
+
+@Entity
+@Table(name = "m_customer")
+data class Customer(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id:String,
+
+    @Column(name = "name", nullable = false)
+    val name:String,
+
+    @Column(name = "birth_date", nullable = false)
+    val birthDate:String,
+
+    @Column(name = "email", nullable = false)
+    val email:String,
+
+    @Column(name = "address", nullable = false)
+    val address:String,
+)
---
+++ src/main/kotlin/com/mcsoftware/ticketo/customer/repository/customerRepository.kt
@@ -0,0 +1,6 @@
+package com.mcsoftware.ticketo.customer.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface customerRepository : JpaRepository<> {
+}
