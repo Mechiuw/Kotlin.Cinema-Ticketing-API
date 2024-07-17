4739f392d7168291e907793a2ddb74499cf5e142
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-18 00:47:28 +0700

    [THEATER][FEATURE] Added theater, wip on ticket and seat

--- src/main/kotlin/com/mcsoftware/ticketo/theater/util/TheaterUpdater.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/theater/util/TheaterUpdater.kt
@@ -2,7 +2,9 @@ package com.mcsoftware.ticketo.theater.util
 
 import com.mcsoftware.ticketo.theater.model.dto.request.TheaterRequest
 import com.mcsoftware.ticketo.theater.model.entity.Theater
+import org.springframework.stereotype.Component
 
+@Component
 class TheaterUpdater {
     fun updaterTheater(entity:Theater,requestUpdate : TheaterRequest):Theater{
         try{
