e6082968a6743de3cebf2000234117a4c3c0db7e
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-18 08:10:59 +0700

    [SEAT][FEATURE] Added Update Function Service

--- src/main/kotlin/com/mcsoftware/ticketo/seat/service/SeatService.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/seat/service/SeatService.kt
@@ -3,11 +3,12 @@ package com.mcsoftware.ticketo.seat.service
 import com.mcsoftware.ticketo.seat.model.dto.request.SeatRequest
 import com.mcsoftware.ticketo.seat.model.dto.response.SeatResponse
 import com.mcsoftware.ticketo.seat.model.entity.Seat
+import java.util.UUID
 
 interface SeatService {
     fun create(request: SeatRequest):SeatResponse;
-    fun update(id:String,request: SeatRequest):SeatResponse;
-    fun delete(id:String);
-    fun getSeat(id:String):SeatResponse;
+    fun update(id:UUID,request: SeatRequest):SeatResponse;
+    fun delete(id:UUID);
+    fun getSeat(id:UUID):SeatResponse;
     fun allSeat():List<Seat>;
 }
--- src/main/kotlin/com/mcsoftware/ticketo/seat/service/impl/SeatServiceImpl.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/seat/service/impl/SeatServiceImpl.kt
@@ -9,7 +9,9 @@ import com.mcsoftware.ticketo.seat.util.SeatConverter
 import com.mcsoftware.ticketo.seat.util.SeatUpdater
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.dao.DataAccessException
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class SeatServiceImpl (
@@ -32,15 +34,28 @@ class SeatServiceImpl (
         }
     }
 
-    override fun update(id: String, request: SeatRequest): SeatResponse {
+    override fun update(id: UUID, request: SeatRequest): SeatResponse {
+        try{
+          val fetchSeat : Optional<Seat> = repo.findById(id)
+            if(fetchSeat.isPresent){
+                return convert.convertToResponse(updater.seatUpdater(fetchSeat.get(),request))
+            } else {
+                throw NotFoundException()
+            }
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException("Invalid input: ${e.message}")
+        } catch (e: DataAccessException) {
+            throw IllegalAccessException("Database error: ${e.message}")
+        } catch (e: Exception) {
+            throw RuntimeException("Unexpected error: ${e.message}")
+        }
+    }
+
+    override fun delete(id: UUID) {
         TODO("Not yet implemented")
     }
 
-    override fun delete(id: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getSeat(id: String): SeatResponse {
+    override fun getSeat(id: UUID): SeatResponse {
         TODO("Not yet implemented")
     }
 
