515c968b572caf644100e052df826d7a6852c47b
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-18 08:18:29 +0700

    [CUSTOMER][IMPROVE] Improved Request

--- src/main/kotlin/com/mcsoftware/ticketo/customer/model/dto/request/CustomerRequest.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/customer/model/dto/request/CustomerRequest.kt
@@ -1,11 +1,16 @@
 package com.mcsoftware.ticketo.customer.model.dto.request
 
+import jakarta.validation.constraints.NotBlank
 import lombok.Data
 
 @Data
 data class CustomerRequest(
+    @field:NotBlank(message = "customer name cannot be null")
     val name: String,
+    @field:NotBlank(message = "customer birth date cannot be null")
     val birthDate: String,
+    @field:NotBlank(message = "customer email cannot be null")
     val email: String,
+    @field:NotBlank(message = "customer address cannot be null")
     val address: String
 )
