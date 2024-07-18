2e6f9f18f9b529925af586eae408cbb024f7c548
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-18 08:08:08 +0700

    [SEAT][FEATURE] Added Create Function Service

--- src/main/kotlin/com/mcsoftware/ticketo/seat/service/impl/SeatServiceImpl.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/seat/service/impl/SeatServiceImpl.kt
@@ -5,16 +5,31 @@ import com.mcsoftware.ticketo.seat.model.dto.response.SeatResponse
 import com.mcsoftware.ticketo.seat.model.entity.Seat
 import com.mcsoftware.ticketo.seat.repository.SeatRepository
 import com.mcsoftware.ticketo.seat.service.SeatService
+import com.mcsoftware.ticketo.seat.util.SeatConverter
+import com.mcsoftware.ticketo.seat.util.SeatUpdater
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataAccessException
 import org.springframework.stereotype.Service
 
 @Service
 class SeatServiceImpl (
     @Autowired
     private val repo:SeatRepository,
+    private val convert:SeatConverter,
+    private val updater: SeatUpdater,
 ): SeatService {
     override fun create(request: SeatRequest): SeatResponse {
-        TODO("Not yet implemented")
+        try {
+            val newSeat = convert.convertToSeat(request)
+            val savedSeat = repo.save(newSeat)
+            return convert.convertToResponse(savedSeat)
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException("Invalid input: ${e.message}")
+        } catch (e: DataAccessException) {
+            throw IllegalAccessException("Database error: ${e.message}")
+        } catch (e: Exception) {
+            throw RuntimeException("Unexpected error: ${e.message}")
+        }
     }
 
     override fun update(id: String, request: SeatRequest): SeatResponse {
