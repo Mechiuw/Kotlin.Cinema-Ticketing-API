f44f48f24cec3ebaa04d5d68534b09e78bb33c6a
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-18 23:31:13 +0700

    [MOVIE][WIP] movie service wip

--- src/main/kotlin/com/mcsoftware/ticketo/movie/service/MovieService.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/movie/service/MovieService.kt
@@ -2,12 +2,14 @@ package com.mcsoftware.ticketo.movie.service
 
 import com.mcsoftware.ticketo.movie.model.dto.request.MovieRequest
 import com.mcsoftware.ticketo.movie.model.dto.response.MovieResponse
+import com.mcsoftware.ticketo.movie.model.entity.Movie
+import java.util.*
 
 
 interface MovieService {
     fun createMovies(request : MovieRequest) :MovieResponse;
-    fun updateMovies(id: String,request: MovieRequest);
-    fun deleteMovies(id: String);
-    fun getMovies(id: String);
-    fun allMovies();
+    fun updateMovies(id: UUID,request: MovieRequest):MovieResponse;
+    fun deleteMovies(id: UUID);
+    fun getMovies(id: UUID):MovieResponse;
+    fun allMovies():List<Movie>;
 }
--- src/main/kotlin/com/mcsoftware/ticketo/movie/service/impl/MovieServiceImpl.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/movie/service/impl/MovieServiceImpl.kt
@@ -2,15 +2,19 @@ package com.mcsoftware.ticketo.movie.service.impl
 
 import com.mcsoftware.ticketo.movie.model.dto.request.MovieRequest
 import com.mcsoftware.ticketo.movie.model.dto.response.MovieResponse
+import com.mcsoftware.ticketo.movie.model.entity.Movie
 import com.mcsoftware.ticketo.movie.repository.MovieRepository
 import com.mcsoftware.ticketo.movie.service.MovieService
 import com.mcsoftware.ticketo.movie.util.MovieConverter
+import com.mcsoftware.ticketo.movie.util.MovieUpdater
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class MovieServiceImpl (
     private val repo : MovieRepository,
     private val convert : MovieConverter,
+    private val updater : MovieUpdater,
 ) : MovieService {
 
     override fun createMovies(request: MovieRequest): MovieResponse {
@@ -23,24 +27,45 @@ class MovieServiceImpl (
         }
     }
 
-    override fun updateMovies(id: String, request: MovieRequest) {
+    override fun updateMovies(id: UUID, request: MovieRequest) :MovieResponse{
         try{
-
-            return
+            val fetchMovie = repo.findById(id).orElseThrow() { RuntimeException() }
+            val updatedMovie = updater.updateMovie(fetchMovie,request)
+            val savedMovie = repo.saveAndFlush(updatedMovie)
+            return convert.convertToResponse(savedMovie)
         } catch (e:Exception){
             throw RuntimeException(e.message)
         }
     }
 
-    override fun deleteMovies(id: String) {
-        TODO("Not yet implemented")
+    override fun deleteMovies(id: UUID) {
+        try{
+            val fetchData = repo.findById(id).orElseThrow() { Exception() }
+            repo.delete(fetchData)
+        } catch (e:Exception){
+            throw RuntimeException(e.message)
+        }
     }
 
-    override fun getMovies(id: String) {
-        TODO("Not yet implemented")
+    override fun getMovies(id: UUID) :MovieResponse{
+        try{
+            val fetchData = repo.findById(id).orElseThrow() { Exception() }
+            return convert.convertToResponse(fetchData)
+        } catch (e:Exception){
+            throw RuntimeException(e.message)
+        }
     }
 
-    override fun allMovies() {
-        TODO("Not yet implemented")
+    override fun allMovies() :List<Movie>{
+        try{
+            val allData = repo.findAll()
+            return if(allData.isNotEmpty()){
+                allData
+            } else {
+                Collections.emptyList()
+            }
+        } catch (e:Exception){
+            throw RuntimeException(e.message)
+        }
     }
 }
