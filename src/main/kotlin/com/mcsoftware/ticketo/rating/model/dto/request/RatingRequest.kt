291f80ec1d6b6e6aa127d292f0d07954375b421d
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-18 08:21:07 +0700

    [THEATER][IMPROVE] Improved Theater Request

--- src/main/kotlin/com/mcsoftware/ticketo/rating/model/dto/request/RatingRequest.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/rating/model/dto/request/RatingRequest.kt
@@ -2,11 +2,14 @@ package com.mcsoftware.ticketo.rating.model.dto.request
 
 import com.mcsoftware.ticketo.movie.model.entity.Movie
 import com.mcsoftware.ticketo.rating.constant.ECode
+import jakarta.validation.constraints.NotBlank
 import lombok.Data
 
 @Data
 data class RatingRequest(
+    @field:NotBlank(message = "rating code cannot be null")
     val code: ECode,
+    @field:NotBlank(message = "rating description cannot be null")
     val description: String,
     val moviesLinked : List<Movie>,
 )
--- src/main/kotlin/com/mcsoftware/ticketo/theater/model/dto/request/TheaterRequest.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/theater/model/dto/request/TheaterRequest.kt
@@ -1,11 +1,15 @@
 package com.mcsoftware.ticketo.theater.model.dto.request
 
+import jakarta.validation.constraints.NotBlank
 import lombok.Data
 import java.util.UUID
 
 @Data
 data class TheaterRequest(
+    @field:NotBlank(message = "rating theater number cannot be null")
     val theaterNumber:String,
+    @field:NotBlank(message = "rating stock seats cannot be null")
     val stockSeats:Int,
+    @field:NotBlank(message = "rating film id cannot be null")
     val filmId:UUID,
 )
