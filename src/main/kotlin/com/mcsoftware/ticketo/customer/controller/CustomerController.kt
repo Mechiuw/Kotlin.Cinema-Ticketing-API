5411b34d67b38cb81cbf498aa4be1fdeb733f3b6
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-19 15:03:16 +0700

    [CUSTOMER][FEATURE] Added Update Controller

--- src/main/kotlin/com/mcsoftware/ticketo/customer/controller/CustomerController.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/customer/controller/CustomerController.kt
@@ -1,25 +1,35 @@
 package com.mcsoftware.ticketo.customer.controller
 
 import com.mcsoftware.ticketo.customer.model.dto.request.CustomerRequest
-import com.mcsoftware.ticketo.customer.model.dto.response.CustomerResponse
 import com.mcsoftware.ticketo.customer.service.interfaces.CustomerService
 import com.mcsoftware.ticketo.json.AppEndpoint
 import com.mcsoftware.ticketo.json.ResponseJSON
+import jakarta.persistence.PreUpdate
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
+import java.util.UUID
 
 @RestController
 @RequestMapping(AppEndpoint.API_CUSTOMER)
 class CustomerController(
     private val service: CustomerService,
-    private val json: ResponseJSON<CustomerResponse>,
+    private val json: ResponseJSON<Any>,
 ) {
     @PostMapping
-    fun create(@RequestBody request: CustomerRequest) :ResponseEntity<ResponseJSON<CustomerResponse>>{
+    fun create(@RequestBody request: CustomerRequest) :ResponseEntity<Any>{
         val createService = service.createCustomer(request)
         val response = json.apply {
             statusCode = HttpStatus.CREATED.value()
         }.useResponse(createService)
         return ResponseEntity(response,HttpStatus.CREATED)
+    }
+
+    @PutMapping(AppEndpoint.PUT_ID)
+    fun update(@PathVariable id:UUID,@RequestBody request:CustomerRequest) :ResponseEntity<Any>{
+        val updateService = service.updateCustomer(id,request)
+        val response = json.apply {
+            statusCode = HttpStatus.OK.value()
+        }.useResponse(updateService)
+        return ResponseEntity(response,HttpStatus.OK)
     }
 }
--- src/main/kotlin/com/mcsoftware/ticketo/json/AppEndpoint.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/json/AppEndpoint.kt
@@ -2,4 +2,14 @@ package com.mcsoftware.ticketo.json
 
 object AppEndpoint {
     const val API_CUSTOMER = "/api/customer";
+    const val API_MOVIE = "/api/movie";
+    const val API_RATING = "/api/rating";
+    const val API_SEAT = "/api/seat";
+    const val API_THEATER = "/api/theater";
+    const val API_TICKET = "/api/ticket";
+
+    const val GET_ID = "/{id}";
+    const val DEL_ID = "/{id}";
+    const val PUT_ID = "/{id}";
+    const val GET_ALL = "/all";
 }
