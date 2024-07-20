24e391eadae4e045ffa49dd47260d4e4dfb1d981
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-20 23:35:15 +0700

    [TICKET][FEATURE] Added feature to controller

--- src/main/kotlin/com/mcsoftware/ticketo/ticket/controller/TicketController.kt
+++ src/main/kotlin/com/mcsoftware/ticketo/ticket/controller/TicketController.kt
@@ -6,7 +6,10 @@ import com.mcsoftware.ticketo.ticket.model.dto.request.TrxTicketRequest
 import com.mcsoftware.ticketo.ticket.service.interfaces.TrxTicketService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -17,13 +20,19 @@ class TicketController(
     private val service : TrxTicketService
 ) {
     @PostMapping
-    fun create(request:TrxTicketRequest) :ResponseEntity<Any>{
+    fun create(@RequestBody request:TrxTicketRequest) :ResponseEntity<Any>{
         val createService = service.create(request)
         val json = ResponseJSON<Any>(HttpStatus.CREATED.value())
         val response = json.useResponse(createService)
         return ResponseEntity(response,HttpStatus.CREATED)
     }
-    fun update(id:UUID,request:TrxTicketRequest) :ResponseEntity<Any>{}
+    @PutMapping(AppEndpoint.PUT_ID)
+    fun update(@PathVariable id:UUID,@RequestBody request:TrxTicketRequest) :ResponseEntity<Any>{
+        val updateService = service.create(request)
+        val json = ResponseJSON<Any>(HttpStatus.OK.value())
+        val response = json.useResponse(updateService)
+        return ResponseEntity(response,HttpStatus.OK)
+    }
     fun delete(id:UUID) :ResponseEntity<Any>{}
     fun getTicket(id:UUID) :ResponseEntity<Any>{}
     fun allTicket() :ResponseEntity<Any>{}
