1d2a866a45590fc1b27717d5c78b1fbc61a1b807
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-20 22:08:07 +0700

    [SEAT][FEATURE] Added Controllers

---
+++ src/main/kotlin/com/mcsoftware/ticketo/seat/controller/SeatController.kt
@@ -0,0 +1,34 @@
+package com.mcsoftware.ticketo.seat.controller
+
+import com.mcsoftware.ticketo.json.AppEndpoint
+import com.mcsoftware.ticketo.seat.model.dto.request.SeatRequest
+import com.mcsoftware.ticketo.seat.service.SeatService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(AppEndpoint.API_SEAT)
+class SeatController(
+    private val service : SeatService
+) {
+    @PostMapping
+    fun create(@RequestBody request:SeatRequest):ResponseEntity<Any>{}
+    @PutMapping(AppEndpoint.PUT_ID)
+    fun update(@PathVariable id:String,@RequestBody request:SeatRequest):ResponseEntity<Any>{}
+    @DeleteMapping(AppEndpoint.DEL_ID)
+    fun delete(@PathVariable id:String):ResponseEntity<Any>{}
+
+    @GetMapping(AppEndpoint.GET_ID)
+    fun getSeat(@PathVariable id:String):ResponseEntity<Any>{}
+    @GetMapping(AppEndpoint.GET_ALL)
+    fun allSeat():ResponseEntity<Any>{}
+    @DeleteMapping
+    fun delAllSeat():ResponseEntity<Any>{}
+}
